f2ccd282ee66595ccb7feb8a4e4a6855583e64e2
LenaSaraieva <lena.saraeva1904@gmail.com>
2023-07-04 12:30:15 +0300

    math operations

---
+++ index.js
@@ -0,0 +1,15 @@
+let number1 =prompt(2)
+let number2 =prompt(2)
+number1 = Number(2); 
+number2 = Number(2);
+const sum= number1 + number2;
+alert (`2 + 2 = ${sum}`)
+
+const difference= number1 - number2;
+alert (`2 - 2 = ${difference}`)
+
+const product= number1 * number2;
+alert (`2 * 2 = ${product}`)
+
+const division= number1 / number2;
+alert (`2 / 2 = ${division}`)
