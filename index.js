0b09864f7113f9224fad41b0cf213b0bf4705fe8
LenaSaraieva <lena.saraeva1904@gmail.com>
2023-07-06 14:56:13 +0300

    old version

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
-let number1 = prompt('Please, enter the first number')
-number1 = Number(2);
-let number2 = prompt('Please, enter the second number')
-number2 = Number(2);
-alert (`
-    User entered ${number1} and ${number2}
-     ${number1} + ${number2} = ${number1 + number2}
-     ${number1} - ${number2} = ${number1 - number2}
-     ${number1} * ${number2} = ${number1 * number2}
-     ${number1} / ${number2} = ${number1 / number2}`);
+let number1 = prompt ("Введіть перше число")
+if (number1 === null || number1 === "")
+{alert ('Ви не ввели перше число'); }
+else {
+    let number2 = prompt ("Введіть друге число")
+    if (number2 === null || number2 === "")
+    {alert ('Ви не ввели друге число'); }
+}
+
+    number1 = Number(2); 
+    number2 = Number(2);
+
+const sum= number1 + number2;
+alert (`2 + 2 = ${sum}`)
+
+const difference= number1 - number2;
+alert (`2 - 2 = ${difference}`)
+
+const product= number1 * number2;
+alert (`2 * 2 = ${product}`)
+
+const division= number1 / number2;
+alert (`2 / 2 = ${division}`)
